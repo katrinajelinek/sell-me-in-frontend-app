db9f7425138f3f92247f97b954f3c731bb89ebac
katrinajelinek <kmrjelinek@gmail.com>
2021-04-07 11:33:50 -0600

    adds vue2 filters dependency and adds ability to multisearch by categories

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,34 @@
 <template>
   <div class="home">
-    <div v-for="post in posts">
+    <div>
+      <multiselect
+        v-model="categoriesFilter"
+        :options="categories"
+        :multiple="true"
+        :close-on-select="false"
+        :clear-on-select="false"
+        :preserve-search="true"
+        placeholder="Search by Categories"
+        label="name"
+        track-by="name"
+        :preselect-first="true"
+      >
+        <template slot="selection" slot-scope="{ values, isOpen }"
+          ><span
+            class="multiselect__single"
+            v-if="values.length &amp;&amp; !isOpen"
+            >{{ values.length }} categories selected</span
+          ></template
+        >
+      </multiselect>
+    </div>
+    <div
+      v-for="post in orderBy(
+        filterBy(filteredPostsByCategories, titleFilter, 'title'),
+        sortAttribute,
+        -1
+      )"
+    >
       <router-link :to="`/posts/${post.id}`"
         ><h1>{{ post.title }}</h1></router-link
       >
@@ -38,16 +66,32 @@
 
 <script>
 import axios from "axios";
+import Multiselect from "vue-multiselect";
+import Vue2Filters from "vue2-filters";
+
 export default {
+  mixins: [Vue2Filters.mixin],
+  components: {
+    Multiselect,
+  },
   data: function() {
     return {
       posts: [],
       categories: [],
+      categoriesFilter: [],
+      values: [],
+      sortAttribute: "created_at",
+      titleFilter: "",
     };
   },
   created: function() {
     this.indexPosts();
     this.indexCategories();
+  },
+  computed: {
+    filteredPostsByCategories() {
+      return this.getByCategory(this.posts, this.categoriesFilter);
+    },
   },
   methods: {
     indexPosts: function() {
@@ -56,11 +100,20 @@ export default {
         this.posts = response.data;
       });
     },
+    setSortAttribute: function(attribute) {
+      this.sortAttribute = attribute;
+    },
     indexCategories: function() {
       axios.get("/api/categories").then((response) => {
         console.log(response.data);
         this.categories = response.data;
       });
+    },
+    getByCategory: function(posts, categoriesFilter) {
+      categoriesFilter.forEach((category) => {
+        posts = this.filterBy(posts, category.name);
+      });
+      return posts;
     },
   },
 };
